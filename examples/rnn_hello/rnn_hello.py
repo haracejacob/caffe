196dd5aac93360bb841b6a1aa84f7fe5143fa6c4
Dong Min Kim <dkrahd12@gmail.com>
2017-04-10 10:56:35 -0700

    modify solver, add rnn_hello.py

---
+++ examples/rnn_hello/rnn_hello.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  5 05:19:10 2017
+
+@author: kim
+"""
+import caffe
+
+model = 'rnn_net.prototxt'
+weights = 'snapshot_iter_20000.caffemodel'
+
+caffe.set_mode_cpu()
+
+net=caffe.Net(model, weights, caffe.TEST)
+res = net.forward()
+
+for idx in range(4) :
+    print res['argmax'][idx], res['label'][idx], res['prob'][idx]
