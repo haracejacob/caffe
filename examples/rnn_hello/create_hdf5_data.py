e9dc4c83698e97bf296d0c66460f22d8b672f06d
Dong Min Kim <dkrahd12@gmail.com>
2017-04-10 09:40:03 -0700

    modify create_hdf5_data.py

--- examples/rnn_hello/create_hdf5_data.py
+++ examples/rnn_hello/create_hdf5_data.py
@@ -10,16 +10,10 @@ import h5py
 import numpy as np
 
 train_filename = 'rnn_data.hdf5'
-#train_filename = 'xor_data2.hdf5'
 #0:h 1:e 2:l 3:o
 data = np.array((0,1,2,2)).reshape((4,1,1))
 clip = np.array((1,2,3,4)).reshape((4,1))
 labels = np.array((1, 2, 2, 3))
-
-#data = np.array(((0,0)))
-#labels = np.array((1))
-#clip = np.array((0, 1, 2, 3))
-# data = f.create_dataset("data", (4,2), dtype='f') # needs float later!
 
 with h5py.File(train_filename, 'w') as f:
   f['data'] = data.astype(np.float32)
